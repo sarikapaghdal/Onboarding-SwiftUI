dddb130b353bb20e5d525f12dafba00212b39734
Dhaval Shekhada <sarika.paghdal1@gmail.com>
2022-02-20 14:55:20 +0100

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Sarika on 20.02.22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
